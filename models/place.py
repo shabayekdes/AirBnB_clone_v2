2998ed4973a07bbd93f8cfb90db7fb1755de0d7b
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-12-21 00:43:27 +0200

    Add places relation

--- models/place.py
+++ models/place.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """ Class Place """
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
-from models.base_model import BaseModel
 
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ Place class inherits from BaseModel """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     amenity_ids = []
--- models/user.py
+++ models/user.py
@@ -2,7 +2,8 @@
 """ User Class """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 class User(BaseModel, Base):
     """
@@ -14,3 +15,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                        backref="user")
