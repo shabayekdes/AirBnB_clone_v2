0f7c8ee815b7af88c0e170384b856188968e2b5a
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-01-12 03:15:52 +0200

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,22 +10,22 @@ env.hosts = ['100.26.210.229', '54.210.107.66']
 def do_deploy(archive_path):
     """Distribute archive to web servers
     """
-    if not os.path.exists(archive_path):
-        return False
-    filename = os.path.basename(archive_path)
-    tem_archive = "/tmp/{}".format(filename)
-    name, ext = os.path.splitext(filename)
-    deploy_dir = "/data/web_static/releases/{}".format(name)
-    try:
-        put(archive_path, tem_archive)
-        run("sudo mkdir -p {}".format(deploy_dir))
-        run("sudo tar -xzf {} -C {}".format(tem_archive, deploy_dir))
-        run("sudo rm {}".format(tem_archive))
-        run("sudo mv {}/web_static/* {}/".format(deploy_dir, deploy_dir))
-        run("sudo rm -rf {}/web_static".format(deploy_dir))
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(deploy_dir))
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
         return True
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return False
+
+    return False
