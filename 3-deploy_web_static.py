2abff63379b4cbb45d2163e54dbcc0478aac863e
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-01-14 21:25:55 +0200

    update

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,8 +3,48 @@
 Fabric script that generates a tgz archive from the contents of the web_static
 folder of the AirBnB Clone repo
 """
-do_pack = __import__('1-pack_web_static')
-do_deploy = __import__('2-do_deploy_web_static')
+from datetime import datetime
+from fabric.api import local, put, run, env
+import os
+
+env.hosts = ['100.26.210.229', '54.210.107.66']
+
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if os.path.isdir("versions") is False:
+            os.makedirs("versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """Distribute archive to web servers
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
+        return True
+
+    return False
 
 def deploy():
     """creates and distributes an archive to the web servers"""
