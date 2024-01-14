78dae5f637b16bf41a6764ee76b2c9cdc101736f
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-01-14 19:44:39 +0200

    Fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,4 +15,4 @@ sudo echo "<html>
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '15i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo service nginx restart
