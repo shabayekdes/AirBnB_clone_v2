add125437317ac42126ade0b53b1c0f0227ff150
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-01-15 00:46:20 +0200

    Update url

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -12,7 +12,7 @@ $nginx_conf = "server {
         index index.html index.htm;
     }
     location /redirect_me {
-        return 301 http://linktr.ee/firdaus_h_salim/;
+        return 301 https://www.youtube.com/watch?v=8P2r0xSXk28;
     }
     error_page 404 /404.html;
     location /404 {
